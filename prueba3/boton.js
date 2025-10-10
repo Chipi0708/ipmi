287d35bcb62456cd9bc482d0b8420b13b2e0e0d4
zoe di lorenzo <zoechipi@gmail.com>
2025-10-10 18:17:21 -0300

    joaco 2

---
+++ prueba3/boton.js
@@ -0,0 +1,7 @@
+function mousePressed (){ 
+  //ramifacaciones
+ if (click (posX, posY, tam)) {PasarPantalla (Pantalla,Pantalla++);}
+else if (click (posXD, posYD, tam, tam)) {PasarPantalla (Pantalla,7); FinalMalo.play();}
+else if (click (posXD2, posYD2, tam, tam)) {PasarPantalla (Pantalla,6);}
+else if (click (posXR, posYR, tam, tam)) {PasarPantalla (Pantalla,0);}
+}
